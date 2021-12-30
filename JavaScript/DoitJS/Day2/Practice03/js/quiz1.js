6f285d8037026873ce3193787b54c981c0b60d0c
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2021-12-30 16:46:37 +0900

    Day2 Quiz

---
+++ JavaScript/DoitJS/Day2/Practice03/js/quiz1.js
@@ -0,0 +1,2 @@
+var content = prompt("내용 입력: ");
+document.write(content);
---
+++ JavaScript/DoitJS/Day2/Practice03/js/quiz2.js
@@ -0,0 +1,4 @@
+var row = prompt("사각형의 가로 값: ");
+var column = prompt("사각형의 세로 값: ");
+var square = row * column;
+alert(`주어진 사각형의 너비: ${square}`);
